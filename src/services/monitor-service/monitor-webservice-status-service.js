6494974c5e1d79b2c6d00882abaaa541a1229806
ManvithSuresh <155379347+ManvithSuresh@users.noreply.github.com>
2024-01-31 12:51:05 +0530

    Update monitor-webservice-status-service.js

--- src/services/monitor-service/monitor-webservice-status-service.js
+++ src/services/monitor-service/monitor-webservice-status-service.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_REST_API_URL = 'http://localhost:9090/api/';
+const BASE_REST_API_URL = 'http://20.235.254.155:9090/api/';
 
 
 export const getMonitorWebServiceStatus= ()=> {
