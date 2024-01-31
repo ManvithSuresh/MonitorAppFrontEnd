fade46cc4fc184df86e2e7f5b71f52e18b83b0fe
ManvithSuresh <155379347+ManvithSuresh@users.noreply.github.com>
2024-01-31 12:52:03 +0530

    Update task-manager-service.js

--- src/services/task-manager-service.js
+++ src/services/task-manager-service.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_REST_API_URL = 'http://localhost:9090/system/';
+const BASE_REST_API_URL = 'http://20.235.254.155:9090/system/';
 
 export const getCPUInfo = ()=> {
     return axios.get(BASE_REST_API_URL+'cpu')
